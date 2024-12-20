f4b8a9e15111ab6356f71b309048adec08e759ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:23:13 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s אומר ב-%s:';
+$lang['* : mandatory fields'] = '*: שדות חובה';
+$lang['Activate rating'] = 'הפעל דירוג';
+$lang['Add link in menu'] = 'הוסף קישור בתפריט';
+$lang['GuestBook'] = 'ספר האורחים';
+$lang['Guests (not registered) permissions :'] = 'הרשאות לאורחים (לא רשומים):';
+$lang['Please enter a message'] = 'אנא הזן הודעה';
+$lang['Please enter your e-mail'] = 'אנא הזן את כתובת האימייל שלך';
+$lang['Please enter your username'] = 'אנא הזן את שם המשתמש שלך';
+$lang['Rate'] = 'דרג';
+$lang['Send'] = 'שלח';
+$lang['Sign the guestbook'] = 'חתום על ספר האורחים';
+$lang['There are %d messages'] = 'יש %d הודעות';
+$lang['Website'] = 'אתר אינטרנט';
+$lang['add comment'] = 'הוסף תגובה';
+$lang['invalid website address'] = 'כתובת אתר לא חוקית';
+$lang['not publicly visible'] = 'לא גלוי לציבור';
+$lang['view comments'] = 'צפה בתגובות';
